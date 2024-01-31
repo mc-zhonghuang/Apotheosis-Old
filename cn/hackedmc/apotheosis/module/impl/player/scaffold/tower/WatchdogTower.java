c10cfac7dee992c52b58c52afa6eb690616d4c91
mc-zhonghuang <mc_zhonghuang@qq.com>
2024-01-31 19:40:53 +0800

    idk

--- cn/hackedmc/apotheosis/module/impl/player/scaffold/tower/WatchdogTower.java
+++ cn/hackedmc/apotheosis/module/impl/player/scaffold/tower/WatchdogTower.java
@@ -23,20 +23,25 @@ public class WatchdogTower extends Mode<Scaffold> {
     @EventLink()
     public final Listener<PreMotionEvent> onPreMotion = event -> {
         if (mc.gameSettings.keyBindJump.isKeyDown() && PlayerUtil.blockNear(2)) {
-            towerTick++;
+            if (MoveUtil.isMoving()) {
+                towerTick++;
 
-            if (mc.thePlayer.onGround)
+                if (mc.thePlayer.onGround)
+                    towerTick = 0;
+
+                mc.thePlayer.motionY = 0.41965;
+                MoveUtil.strafe(0.265);
+
+                if (towerTick == 1)
+                    mc.thePlayer.motionY = 0.33;
+                else if (towerTick == 2)
+                    mc.thePlayer.motionY = 1 - mc.thePlayer.posY % 1;
+                else if (towerTick >= 3)
+                    towerTick = 0;
+            } else {
                 towerTick = 0;
-
-            mc.thePlayer.motionY = 0.41965;
-            MoveUtil.strafe(0.265);
-
-            if (towerTick == 1)
-                mc.thePlayer.motionY = 0.33;
-            else if (towerTick == 2)
-                mc.thePlayer.motionY = 1 - mc.thePlayer.posY % 1;
-            else if (towerTick >= 3)
-                towerTick = 0;
+                if (mc.thePlayer.onGround) mc.thePlayer.jump();
+            }
         } else {
             towerTick = 0;
         }
