5efd909daf50329f8e6e936a13d91ec95c021020
mc-zhonghuang <mc_zhonghuang@qq.com>
2024-02-11 01:19:02 +0800

    私聊

--- cn/hackedmc/apotheosis/command/impl/IRC.java
+++ cn/hackedmc/apotheosis/command/impl/IRC.java
@@ -36,8 +36,24 @@ public final class IRC extends Command {
                     ChatUtil.displayNoPrefix("§bUsername§r -> " + key + " §dGameId§r -> " + value);
                 });
                 ChatUtil.displayNoPrefix("·===================·");
-            } else if ((args[1].equalsIgnoreCase("ban") || args[1].equalsIgnoreCase("chat") || args[1].equalsIgnoreCase("mute") || args[1].equalsIgnoreCase("unmute")) && args.length > 2) {
-                if (args[1].equalsIgnoreCase("mute")) {
+            } else if (((args[1].equalsIgnoreCase("ban") || args[1].equalsIgnoreCase("chat") || args[1].equalsIgnoreCase("mute") || args[1].equalsIgnoreCase("unmute")) && args.length > 2) || (args[1].equalsIgnoreCase("message") && args.length > 3)) {
+                if (args[1].equalsIgnoreCase("message")) {
+                    final String message = StringUtils.join(Arrays.stream(args).skip(3).collect(Collectors.toList()), " ");
+                    final int length = message.length();
+                    if (System.currentTimeMillis() < Fucker.time || Fucker.time == -1) {
+                        ChatUtil.displayIRC("§c§l错误§r >> 你正在被禁言中！");
+                    } else if (((Fucker.rank == Fucker.Rank.NORMAL && length > 15) || (Fucker.rank == Fucker.Rank.VIP && length > 20) || (Fucker.rank == Fucker.Rank.SVIP && length > 30) || (Fucker.rank == Fucker.Rank.MOD && length > 40)) && (length > Fucker.maxChat && Fucker.maxChat != -1)) {
+                        ChatUtil.displayIRC("§c§l错误§r >> 发送的信息过长！");
+                    } else {
+                        final JsonObject jsonObject = new JsonObject();
+                        jsonObject.addProperty("Packet", "Message");
+                        jsonObject.addProperty("User", Fucker.name);
+                        jsonObject.addProperty("ToUser", args[2]);
+                        jsonObject.addProperty("Text", message);
+
+                        ByteUtil.send(Fucker.channel, CryptUtil.DES.encrypt(jsonObject.toString(), Fucker.username, Fucker.password));
+                    }
+                } else if (args[1].equalsIgnoreCase("mute")) {
                     final JsonObject jsonObject = new JsonObject();
                     jsonObject.addProperty("Packet", "Mute");
                     jsonObject.addProperty("User", Fucker.name);
