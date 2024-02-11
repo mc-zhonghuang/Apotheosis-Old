50272a020f227c398233d3c2f7ac8e66b86818df
Gaimoy <10510544@qq.com>
2024-02-11 19:02:40 +0800

    额....

---
+++ cn/hackedmc/apotheosis/module/impl/other/MusicPlayer.java
@@ -0,0 +1,15 @@
+package cn.hackedmc.apotheosis.module.impl.other;
+
+import cn.hackedmc.apotheosis.api.Rise;
+import cn.hackedmc.apotheosis.module.Module;
+import cn.hackedmc.apotheosis.module.api.Category;
+import cn.hackedmc.apotheosis.module.api.ModuleInfo;
+
+@Rise
+@ModuleInfo(name = "MusicPlayer", description = "Play music and display lyrics", category = Category.OTHER)
+public class MusicPlayer extends Module {
+    public void onEnable() {
+        System.out.println("吕子乔写个musicplayer给我看!!!");
+    }
+    //吕子乔写个给我看吕子乔写个给我看吕子乔写个给我看吕子乔写个给我看吕子乔写个给我看吕子乔写个给我看吕子乔写个给我看吕子乔写个给我看吕子乔写个给我看吕子乔写个给我看吕子乔写个给我看吕子乔写个给我看吕子乔写个给我看吕子乔写个给我看
+}
